51932f7e9d3fbc4af168dbe602f83f8d0442d6c3
jj165332 <139018500+jj165332@users.noreply.github.com>
2023-07-08 18:42:17 -0400

    Update Dude.lua

--- Dude.lua
+++ Dude.lua
@@ -1,14 +1,23 @@
+local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local RolesSequenceMap = ReplicatedStorage:WaitForChild("RolesSequenceMap")
 
-for _, player in ipairs(game:GetService("Players"):GetPlayers()) do
-    local roleFolder = RolesSequenceMap:FindFirstChild(player.Name)
-    if roleFolder then
-        for _, role in ipairs(roleFolder:GetChildren()) do
-            local roleName = role.Name
-            if roleName == "Innocent" or roleName == "Mafia" or roleName == "Detective" or roleName == "Doctor" then
-                print(player.Name .. "'s role: " .. roleName)
-            end
+local roleFolder = ReplicatedStorage:WaitForChild("Scripts"):WaitForChild("Modules"):WaitForChild("roles")
+
+local function displayRoleName()
+    for _, player in ipairs(Players:GetPlayers()) do
+        local roleModule = roleFolder:FindFirstChild(player.Name)
+        if roleModule then
+            local role = require(roleModule)
+            local roleLabel = Instance.new("TextLabel")
+            roleLabel.Text = role
+            roleLabel.Size = UDim2.new(0, 100, 0, 20)
+            roleLabel.Position = UDim2.new(0, 0, 0, -30)
+            roleLabel.TextColor3 = Color3.new(1, 1, 1)
+            roleLabel.BackgroundTransparency = 1
+            roleLabel.Font = Enum.Font.SourceSansBold
+            roleLabel.Parent = player.Character.Head
         end
     end
 end
+
+displayRoleName()
