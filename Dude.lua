9b09224760411f84875607354e19e2d1b10d44d0
jj165332 <139018500+jj165332@users.noreply.github.com>
2023-07-09 12:48:39 -0400

    Update Dude.lua

--- Dude.lua
+++ Dude.lua
@@ -1,23 +1,129 @@
-local Players = game:GetService("Players")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local roleFolder = ReplicatedStorage:WaitForChild("Scripts"):WaitForChild("Modules"):WaitForChild("roles")
-
-local function displayRoleName()
-    for _, player in ipairs(Players:GetPlayers()) do
-        local roleModule = roleFolder:FindFirstChild(player.Name)
-        if roleModule then
-            local role = require(roleModule)
-            local roleLabel = Instance.new("TextLabel")
-            roleLabel.Text = role
-            roleLabel.Size = UDim2.new(0, 100, 0, 20)
-            roleLabel.Position = UDim2.new(0, 0, 0, -30)
-            roleLabel.TextColor3 = Color3.new(1, 1, 1)
-            roleLabel.BackgroundTransparency = 1
-            roleLabel.Font = Enum.Font.SourceSansBold
-            roleLabel.Parent = player.Character.Head
-        end
-    end
+function sendmsg(msg)
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+Text = msg;
+})
 end
 
-displayRoleName()
+local rolesandcolors = {
+["Mafia"] = Color3.fromRGB(255, 0, 0),
+["Vampire"] = Color3.fromRGB(0, 255, 0),
+["Coven"] = Color3.fromRGB(255, 255, 0),
+["Werewolf"] = Color3.fromRGB(64, 64, 64),
+['Bodyguard'] = Color3.fromRGB(0, 0, 255),
+['Medic'] = Color3.fromRGB(0, 0, 255),
+['Guardian'] = Color3.fromRGB(0, 0, 255),
+['Vigilante'] = Color3.fromRGB(255, 0, 255), -- Changed color to pink
+['Liberator'] = Color3.fromRGB(0, 255, 255), -- Changed color to cyan
+['Veteran'] = Color3.fromRGB(0, 0, 255),
+['Bounty Hunter'] = Color3.fromRGB(255, 165, 0), -- Changed color to orange
+}
+
+local gunsandroles = {
+["Shield"] = "Bodyguard",
+["Knife"] = "Killer",
+["GuardianSword"] = "Guardian",
+["Revolver"] = "Vigilante",
+["Katana"] = "Assassin",
+["ShieldK"] = "Liberator",
+["Medkit"] = "Medic",
+["Pistol"] = "Veteran",
+["Winchester"] = "Bounty Hunter",
+}
+
+function bypac()
+local ACRemote = game:GetService("ReplicatedStorage").Remotes.FinishAudio
+local ACBypass
+ACBypass = hookmetamethod(game, "__namecall", function(...)
+local method = getnamecallmethod()
+local args = { ... }
+    if not checkcaller() then
+        if typeof(self) == "Instance" and self == ACRemote and method == "FireServer" then
+            return wait(9e9)
+        end
+    end
+
+    return ACBypass(...)
+end)
+end
+
+sendmsg("BOXTON CLI")
+sendmsg("[CLI] Bypassing anticheat")
+wait(1)
+bypac()
+wait()
+sendmsg("[CLI] Anticheat bypassed!")
+wait()
+sendmsg("[CLI] Snooping for roles..")
+
+for i, v in pairs(game:GetService("Workspace").Game:GetChildren()) do
+local folder = v.Name
+v.ChildAdded:Connect(function(p)
+local plr = game.Players:FindFirstChild(p.Name)
+if folder == "Mafia" or folder == "Vampire" or folder == "Coven" or folder == "Medium" then
+if not plr:GetAttribute("NSXFA") then
+plr:SetAttribute("NSXFA", folder)
+sendmsg('[' .. plr:GetAttribute("NSXFA") .. "] (" .. plr.PlayerData.DisplayName.Value .. ")[" .. plr.PlayerData.Number.Value .. "] has been exposed \n")
+end
+end
+    p.ChildAdded:Connect(function(ai)
+        if ai.Name == "WerewolfVal" then 
+            if not plr:GetAttribute("NSXFA") then 
+                plr:SetAttribute("NSXFA", "Werewolf")
+                sendmsg('[' .. plr:GetAttribute("NSXFA") .. "] (" .. plr.PlayerData.DisplayName.Value  .. ")[" .. plr.PlayerData.Number.Value  ..  "] has been exposed \n")
+            end 
+        end 
+    end)
+end)
+end
+
+game.Players.DescendantAdded:Connect(function(t)
+if t:IsA("Tool") then
+local plr = t.Parent.Parent
+local tool = t
+if not game.Players:FindFirstChild(t.Parent.Parent.Name):GetAttribute("NSXFA") then
+if gunsandroles[t.Name] ~= nil then
+plr:SetAttribute("NSXFA", gunsandroles[t.Name])
+end
+        wait()
+        sendmsg("[" .. (plr:GetAttribute("NSXFA") or "Unknown") .. "] (" .. plr.PlayerData.DisplayName.Value .. ")[" .. plr.PlayerData.Number.Value  ..  "] was found with " .. tool.Name .. '\n')
+    end 
+end 
+end)
+
+for i, plr in pairs(game.Players:GetChildren()) do
+local v = plr
+if plr:GetAttribute("NSXFA") then
+sendmsg('[' .. plr:GetAttribute("NSXFA") .. "] (" .. plr.PlayerData.DisplayName.Value .. ")[" .. plr.PlayerData.Number.Value .. "] has been exposed \n")
+end
+spawn(function()
+    while wait() do 
+        local charr = v.Character or v.CharacterAdded:Wait()
+        local oh = charr:FindFirstChild("Overhead", true)
+        
+        if oh then 
+            oh.AlwaysOnTop = true
+            
+            if v == game.Players.LocalPlayer then 
+                oh.PlayerName.Text = '[' .. plr.PlayerData.Number.Value .. '] ' .. plr.PlayerData.DisplayName.Value
+                oh.PlayerName.TextColor3 = Color3.fromRGB(255, 80, 192)
+            elseif v:GetAttribute("NSXFA") ~= nil and rolesandcolors[v:GetAttribute("NSXFA")] ~= nil then 
+                oh.PlayerName.Text = '[' .. plr.PlayerData.Number.Value .. '] ' .. plr.PlayerData.DisplayName.Value
+                oh.PlayerName.TextColor3 = rolesandcolors[v:GetAttribute("NSXFA")]
+            else
+                oh.PlayerName.Text = '[' .. plr.PlayerData.Number.Value .. '] ' .. plr.PlayerData.DisplayName.Value
+                oh.PlayerName.TextColor3 = Color3.fromRGB(255, 255, 255)
+            end 
+        end 
+    end 
+end)
+end
+
+-- Print everyone's role
+sendmsg("[CLI] Roles:")
+for i, plr in ipairs(game.Players:GetPlayers()) do
+    local role = plr:GetAttribute("NSXFA")
+    if role then
+        wait(0.1) -- Delay before printing each player's role
+        sendmsg('[' .. role .. '] (' .. plr.Name .. ')[' .. plr.UserId .. ']')
+    end
+end
