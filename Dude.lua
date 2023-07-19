a0a867c45359ef3cf08c4ef982946ce0a4e1c526
jj165332 <139018500+jj165332@users.noreply.github.com>
2023-07-19 19:59:01 -0400

    Update Dude.lua

--- Dude.lua
+++ Dude.lua
@@ -11,11 +11,10 @@ end
 function CreateEsp(Player)
     -- Your existing CreateEsp function here...
 
-    local BoxOutline, NameOutline = Drawing.new("Square"), Drawing.new("Text")
+    local NameOutline = Drawing.new("Text")
 
     local PlayerData = {
         Player = Player,
-        BoxOutline = BoxOutline,
         NameOutline = NameOutline,
         -- Rest of your existing PlayerData...
     }
@@ -28,28 +27,12 @@ function CreateEsp(Player)
         if Config.Names then
             -- Update the NameOutline position along with the display name
             NameOutline.Text = Player.DisplayName -- Set the text of the NameOutline to the player's display name
-            NameOutline.Size = 15 -- Set the font size of the NameOutline (adjust as needed)
-            NameOutline.Color = Color3.fromRGB(255, 255, 255) -- Set the color of the NameOutline (white in this example)
             NameOutline.Position = Vector2.new(Player.Character.Head.Position.X, Player.Character.Head.Position.Y) -- Set the position of the NameOutline
 
             -- Rest of your existing Name update logic...
         else
             NameOutline.Visible = false
             PlayerData.Visible = false
-        end
-
-        if Config.Box then
-            -- Update the BoxOutline position and size along with the box
-            BoxOutline.Visible = IsVisible
-            BoxOutline.Color = Config.BoxOutlineColor
-            BoxOutline.Size = Vector2.new(width, height)
-            BoxOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2, Target2dPosition.Y - Box.Size.Y / 2)
-            BoxOutline.Thickness = 2  -- Adjust the outline thickness as needed
-            BoxOutline.Transparency = 1  -- Set outline transparency to 1 (fully visible)
-
-            -- Rest of your existing Box update logic...
-        else
-            BoxOutline.Visible = false
         end
     end)
 end
