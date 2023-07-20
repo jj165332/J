8ea3b63d3364c91a2a0565bfde4a396d558f6693
jj165332 <139018500+jj165332@users.noreply.github.com>
2023-07-19 20:26:33 -0400

    Update Dude.lua

--- Dude.lua
+++ Dude.lua
@@ -1,124 +1,99 @@
--- made by rang#2415 or https://v3rmillion.net/member.php?action=profile&uid=1906262
 local Config = {
-    Box               = false,
-    BoxOutline        = true,
-    BoxColor          = Color3.fromRGB(255,255,255),
-    BoxOutlineColor   = Color3.fromRGB(0,0,0),
-    HealthBar         = false,
-    HealthBarSide     = "Left", -- Left,Bottom,Right
-    Names             = false,
-    NamesOutline      = false,
-    NamesColor        = Color3.fromRGB(255,255,255),
-    NamesOutlineColor = Color3.fromRGB(0,0,0),
-    NamesFont         = 2, -- 0,1,2,3
-    NamesSize         = 13
+    OutlineEnabled    = true,
+    OutlineColor      = Color3.fromRGB(255, 0, 0),
+    Names             = true,
+    NamesColor        = Color3.fromRGB(255, 255, 255),
+    NamesFont         = Enum.Font.SourceSans,
+    NamesSize         = 10,
 }
 
 function CreateEsp(Player)
-    local Box,BoxOutline,Name,HealthBar,HealthBarOutline = Drawing.new("Square"),Drawing.new("Square"),Drawing.new("Text"),Drawing.new("Square"),Drawing.new("Square")
-    local Updater = game:GetService("RunService").RenderStepped:Connect(function()
-    if Player.Character ~= nil and Player.Character:FindFirstChild("Humanoid") ~= nil and Player.Character:FindFirstChild("HumanoidRootPart") ~= nil and Player.Character.Humanoid.Health > 0 and Player.Character:FindFirstChild("Head") ~= nil then
-            local Target2dPosition,IsVisible = workspace.CurrentCamera:WorldToViewportPoint(Player.Character.HumanoidRootPart.Position)
-            local scale_factor = 1 / (Target2dPosition.Z * math.tan(math.rad(workspace.CurrentCamera.FieldOfView * 0.5)) * 2) * 100
-            local width, height = math.floor(40 * scale_factor), math.floor(60 * scale_factor)
-            if Config.Box then
-                Box.Visible = IsVisible
-                Box.Color = Config.BoxColor
-                Box.Size = Vector2.new(width,height)
-                Box.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2)
-                Box.Thickness = 1
-                Box.ZIndex = 69
-               if Config.BoxOutline then
-    BoxOutline.Visible = IsVisible
-    BoxOutline.Color = Config.BoxOutlineColor
-    BoxOutline.Size = Vector2.new(width, height)
-    BoxOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2 - 1, Target2dPosition.Y - Box.Size.Y / 2 - 1) -- Subtract 1 from both X and Y positions
-    BoxOutline.Thickness = 3
-    BoxOutline.ZIndex = 1
-else
-    BoxOutline.Visible = false
-end
-
-            else
-                Box.Visible = false
-                BoxOutline.Visible = false
-            end
-            if Config.Names then
-                Name.Visible = IsVisible
-                Name.Color = Config.NamesColor
-                Name.Text = Player.DisplayName.." "..math.floor((workspace.CurrentCamera.CFrame.p - Player.Character.HumanoidRootPart.Position).magnitude).."m"
-                Name.Center = true
-                Name.Outline = Config.NamesOutline
-                Name.OutlineColor = Config.NamesOutlineColor
-                Name.Position = Vector2.new(Target2dPosition.X,Target2dPosition.Y - height * 0.5 + -15)
-                Name.Font = Config.NamesFont
-                Name.Size = Config.NamesSize
-            else
-                Name.Visible = false
-            end
-            if Config.HealthBar then
-                HealthBarOutline.Visible = IsVisible
-                HealthBarOutline.Color = Color3.fromRGB(0,0,0)
-                HealthBarOutline.Filled = true
-                HealthBarOutline.ZIndex = 1
+    local Line, Name = Drawing.new("Line"), Drawing.new("Text")
     
-                HealthBar.Visible = IsVisible
-                HealthBar.Color = Color3.fromRGB(255,0,0):lerp(Color3.fromRGB(0,255,0), Player.Character:FindFirstChild("Humanoid").Health/Player.Character:FindFirstChild("Humanoid").MaxHealth)
-                HealthBar.Thickness = 1
-                HealthBar.Filled = true
-                HealthBar.ZIndex = 69
-                if Config.HealthBarSide == "Left" then
-                    HealthBarOutline.Size = Vector2.new(2,height)
-                    HealthBarOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2) + Vector2.new(-3,0)
-                    
-                    HealthBar.Size = Vector2.new(1,-(HealthBarOutline.Size.Y - 2) * (Player.Character:FindFirstChild("Humanoid").Health/Player.Character:FindFirstChild("Humanoid").MaxHealth))
-                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(1,-1 + HealthBarOutline.Size.Y)
-                elseif Config.HealthBarSide == "Bottom" then
-                    HealthBarOutline.Size = Vector2.new(width,3)
-                    HealthBarOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2) + Vector2.new(0,height + 2)
-
-                    HealthBar.Size = Vector2.new((HealthBarOutline.Size.X - 2) * (Player.Character:FindFirstChild("Humanoid").Health/Player.Character:FindFirstChild("Humanoid").MaxHealth),1)
-                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(1,-1 + HealthBarOutline.Size.Y)
-                elseif Config.HealthBarSide == "Right" then
-                    HealthBarOutline.Size = Vector2.new(2,height)
-                    HealthBarOutline.Position = Vector2.new(Target2dPosition.X - Box.Size.X / 2,Target2dPosition.Y - Box.Size.Y / 2) + Vector2.new(width + 1,0)
-                    
-                    HealthBar.Size = Vector2.new(1,-(HealthBarOutline.Size.Y - 2) * (Player.Character:FindFirstChild("Humanoid").Health/Player.Character:FindFirstChild("Humanoid").MaxHealth))
-                    HealthBar.Position = HealthBarOutline.Position + Vector2.new(1,-1 + HealthBarOutline.Size.Y)
-                end
+    local function IsVisible(part)
+        local camera = workspace.CurrentCamera
+        local viewVector = camera.CFrame.lookVector
+        local partPos, isVisible = camera:WorldToViewportPoint(part.Position)
+        local ray = Ray.new(camera.CFrame.p, (part.Position - camera.CFrame.p).unit * 3000)
+        local part, position = workspace:FindPartOnRayWithIgnoreList(ray, {camera, Player.Character})
+        if part then
+            local _, pos = camera:WorldToViewportPoint(position)
+            if (pos - partPos).magnitude < 5 then
+                isVisible = true
             else
-                HealthBar.Visible = false
-                HealthBarOutline.Visible = false
-            end
-        else
-            Box.Visible = false
-            BoxOutline.Visible = false
-            Name.Visible = false
-            HealthBar.Visible = false
-            HealthBarOutline.Visible = false
-            if not Player then
-                Box:Remove()
-                BoxOutline:Remove()
-                Name:Remove()
-                HealthBar:Remove()
-                HealthBarOutline:Remove()
-                Updater:Disconnect()
+                isVisible = false
             end
         end
-    end)
+        return isVisible, partPos
+    end
+    
+    local function DrawEsp()
+        if Player.Character and Player.Character:FindFirstChild("Humanoid") and Player.Character:FindFirstChild("HumanoidRootPart") and Player.Character.Humanoid.Health > 0 and Player.Character:FindFirstChild("Head") then
+            local isPlayerVisible, playerPosition = IsVisible(Player.Character.Head)
+            if isPlayerVisible then
+                local headPos = workspace.CurrentCamera:WorldToViewportPoint(Player.Character.Head.Position)
+                local rootPos = workspace.CurrentCamera:WorldToViewportPoint(Player.Character.HumanoidRootPart.Position)
+                local distance = (Vector2.new(headPos.X, headPos.Y) - Vector2.new(rootPos.X, rootPos.Y)).magnitude
+                
+                if Config.OutlineEnabled then
+                    Line.Visible = true
+                    Line.From = Vector2.new(rootPos.X, rootPos.Y)
+                    Line.To = Vector2.new(headPos.X, headPos.Y)
+                    Line.Color = Config.OutlineColor
+                    Line.Thickness = 1
+                    Line.Transparency = 1
+                    Line.Visible = true
+                else
+                    Line.Visible = false
+                end
+                
+                if Config.Names then
+                    Name.Visible = true
+                    Name.Position = Vector2.new(rootPos.X, headPos.Y)
+                    Name.Color = Config.NamesColor
+                    Name.Center = true
+                    Name.Outline = true
+                    Name.OutlineColor = Color3.new(0, 0, 0)
+                    Name.Font = Config.NamesFont
+                    Name.Size = Config.NamesSize
+                    Name.Text = Player.Name .. " (" .. math.floor((workspace.CurrentCamera.CFrame.p - Player.Character.HumanoidRootPart.Position).magnitude) .. "m)"
+                else
+                    Name.Visible = false
+                end
+            else
+                Line.Visible = false
+                Name.Visible = false
+            end
+        else
+            Line.Visible = false
+            Name.Visible = false
+        end
+    end
+    
+    game:GetService("RunService").RenderStepped:Connect(DrawEsp)
 end
 
-for _,v in pairs(game:GetService("Players"):GetPlayers()) do
-   if v ~= game:GetService("Players").LocalPlayer then
-      CreateEsp(v)
-      v.CharacterAdded:Connect(CreateEsp(v))
-   end
+for _, v in ipairs(game:GetService("Players"):GetPlayers()) do
+    if v ~= game:GetService("Players").LocalPlayer then
+        CreateEsp(v)
+        v.CharacterAdded:Connect(function()
+            wait(1)
+            if v.Character then
+                CreateEsp(v)
+            end
+        end)
+    end
 end
 
 game:GetService("Players").PlayerAdded:Connect(function(v)
-   if v ~= game:GetService("Players").LocalPlayer then
-      CreateEsp(v)
-      v.CharacterAdded:Connect(CreateEsp(v))
-   end
+    if v ~= game:GetService("Players").LocalPlayer then
+        v.CharacterAdded:Connect(function()
+            wait(1)
+            if v.Character then
+                CreateEsp(v)
+            end
+        end)
+    end
 end)
+
 return Config
